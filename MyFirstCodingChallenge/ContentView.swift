997633cfc068d4b8530e8b79434f17b14fe33115
Sherrane Watson <Sherranewatson98@gmail.com>
2022-08-29 20:38:14 -0400

    first commit

---
+++ MyFirstCodingChallenge/ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView.swift
+//  MyFirstCodingChallenge
+//
+//  Created by Sherrane Watson on 8/16/22.
+//
+let albums =  ["Red", "Fearless"]
+
---
+++ MyFirstCodingChallenge/MyFirstCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstCodingChallengeApp.swift
+//  MyFirstCodingChallenge
+//
+//  Created by Sherrane Watson on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
